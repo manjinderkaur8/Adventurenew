ecf1accc56c33d38f90dd6de48ab39e9d59e103c
manjinderkaur8 <108488447+manjinderkaur8@users.noreply.github.com>
2023-08-16 00:18:17 +0530

    12-18AM 16-08-2023

--- javascript/index.js
+++ javascript/index.js
@@ -22,7 +22,7 @@ function selectOption(option) {
   selectedOption = option;
 }
 
-function updateTravelClass() {
+function updateTravelClass(event) {
   passengerValue =
     parseInt(document.getElementById("passengerValue").value, 10) || 0;
   classValue = document.getElementById("classValue").value;
@@ -71,18 +71,59 @@ var swiper = new Swiper(".mySwiper", {
 // setInterval(function() { ObserveInputValue($('#fromCity').val()); }, 100);
 
 const key = "iTrn-p6BQN7ru9whsXaTUFby8wLILmvq";
+const client_id = "yFYG6GTjtwTv0av5R9azmzCg0vkIgLA6";
+const client_secret = "Hs1E44zzAC6fwePg";
+// Access Token is here just use it as authToken in request
+var authToken = "";
+// Variables
+var destinationLocation = "";
+var arrivalLocation = "";
+var departureDate = "";
+var arrivalDate = "";
+var traveladult= "";
+
+const AuthUser = async () => {
+  try {
+    const response = await fetch(
+      `https://test.api.amadeus.com/v1/security/oauth2/token`,
+      {
+        method: "POST",
+        headers: {"Content-Type": "application/x-www-form-urlencoded"},
+        body: new URLSearchParams({
+          client_id: client_id,
+          client_secret: client_secret,
+          grant_type: "client_credentials"
+        })
+      }
+    );
+    const data = await response.json();
+    authToken = data.access_token;
+    console.log(authToken);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+window.onload = async () => {
+  await AuthUser();
+  console.log(authToken);
+};
 
 const getFlights = (event) => {
   event.preventDefault();
-
-  const fromCity = document.getElementById("fromCity").value;
-  const toCity = document.getElementById("toCity").value;
-  const dateFrom = document.getElementById("dateFrom").value;
-  const dateTo = document.getElementById("dateTo").value;
-  const travelClass = document.getElementById("travelClass").value;
+  const loading = document.getElementById("loading");
+  loading.style.display = "flex";
+  destinationLocation = document.getElementById("fromCity").value;
+  arrivalLocation = document.getElementById("toCity").value;
+  departureDate = document.getElementById("dateFrom").value;
+  arrivalDate = document.getElementById("dateTo").value;
+  traveladult = document.getElementById("travelClass").value;
 
   if (fromCity !== "" && toCity !== "" && dateFrom !== "" && dateTo !== "") {
     sendRequest(fromCity, toCity, dateFrom, dateTo, travelClass);
+  /*   console.log(
+      `${fromCity}, ${toCity}, ${dateFrom}, ${dateTo}, ${travelClass}`
+    ); */
   }
 };
 
@@ -123,14 +164,19 @@ var sendMail = () => {
 };
 
 sendRequest = (fromCity, toCity, dateFrom, dateTo, travelClass) => {
+
+getData();
+ 
+};
+
+function getData(){
   fetch(
-    `https://api.tequila.kiwi.com/v2/search?fly_from=${fromCity}&fly_to=${toCity}&date_from=${dateFrom}&date_to=${dateTo}`,
+    `https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${destinationLocation}&destinationLocationCode=${arrivalLocation}&departureDate=${departureDate}&returnDate=${arrivalDate}&adults=2`,
     {
       method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        apikey: key,
-      },
+      headers:  new URLSearchParams( {
+        "Authorization": `Bearer ${authToken}`,
+      }),
     }
   )
     .then((response) => response.json())
@@ -139,4 +185,69 @@ sendRequest = (fromCity, toCity, dateFrom, dateTo, travelClass) => {
       window.location.href = "flights.html";
     })
     .catch((err) => console.error(err));
+}
+// Function to fetch airport locations from Tequila Kiwi API
+async function fetchAirportLocations(query, dropdownId) {
+
+
+
+  const response = await fetch(
+    `https://test.api.amadeus.com/v1/reference-data/locations?subType=CITY,AIRPORT&keyword=${query}`,
+    {
+      method: "GET",
+      headers:  new URLSearchParams( {
+        "Authorization": `Bearer ${authToken}`,
+      }),
+    }
+  );
+
+  const data = await response.json();
+  const dropdown = document.getElementById(dropdownId);
+
+  // Clear existing suggestions
+  dropdown.innerHTML = "";
+
+  // Add location suggestions to the dropdown
+  data.data.forEach((location) => {
+    if (location.iataCode) { // Ensure the location has an iataCode
+      const listItem = document.createElement("li");
+      listItem.textContent = `${location.iataCode} - ${location.name}`;
+      listItem.onclick = () => {
+        document.getElementById(dropdownId.replace("Dropdown", "")).value =
+          location.iataCode;
+        dropdown.innerHTML = "";
+      };
+      dropdown.appendChild(listItem);
+    }
+  });
+}
+
+// Event listeners for "From" and "To" input fields
+document.getElementById("fromCity").addEventListener("input", function () {
+  // console.log(document.getElementById("fromCity"));
+  const query = this.value.trim();
+  if (query !== "") {
+    
+    fetchAirportLocations(query, "fromCityDropdown");
+  }
+});
+
+document.getElementById("toCity").addEventListener("input", function () {
+  const query = this.value.trim();
+  if (query !== "") {
+    fetchAirportLocations(query, "toCityDropdown");
+  }
+});
+
+function toggleDateInputs(enableRoundTrip) {
+  const dateFromInput = document.getElementById("dateFrom");
+  const dateToInput = document.getElementById("dateTo");
+
+  dateFromInput.disabled = false;
+  dateToInput.disabled = !enableRoundTrip; 
+}
+
+window.onload = function () {
+  // By default, when the window loads, "One-way" is selected, so disable the "To Date" input
+  toggleDateInputs(false);
 };
